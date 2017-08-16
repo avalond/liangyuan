0bc1116be42541c9c1d77951ea0bd8aa99315676
kevin duan <agonyice0115@gmail.com>
2017-08-16 13:20:32 +0800

    update demo

---
+++ app/src/main/java/com/xiaan/liangyuan/liangyuanapp/LiangYuanApplication.java
@@ -0,0 +1,10 @@
+package com.xiaan.liangyuan.liangyuanapp;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by kevin on 8/16/17.
+ */
+
+public class LiangYuanApplication extends MultiDexApplication{
+}
