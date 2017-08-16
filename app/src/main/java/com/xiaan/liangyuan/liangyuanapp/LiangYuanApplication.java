ea00dff45e85e1abcfcc29da27a1cdb877e4f80f
kevin duan <agonyice0115@gmail.com>
2017-08-16 14:04:41 +0800

    update demo

--- app/src/main/java/com/xiaan/liangyuan/liangyuanapp/LiangYuanApplication.java
+++ app/src/main/java/com/xiaan/liangyuan/liangyuanapp/LiangYuanApplication.java
@@ -3,11 +3,14 @@ package com.xiaan.liangyuan.liangyuanapp;
 import android.content.Context;
 import android.support.multidex.MultiDexApplication;
 
+import com.xiaan.liangyuan.liangyuanapp.utils.LoggerUtils;
+
 /**
  * Created by kevin on 8/16/17.
  */
 
 public class LiangYuanApplication extends MultiDexApplication {
+    private static final String TAG = LiangYuanApplication.class.getSimpleName();
 
     private static Context mContext;
     private String versionName;
@@ -20,7 +23,7 @@ public class LiangYuanApplication extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-
+        LoggerUtils.i(TAG, "application is on created");
         mContext = getApplicationContext();
         versionCode = 0;
         versionName = null;
--- app/src/main/java/com/xiaan/liangyuan/liangyuanapp/MainActivity.java
+++ app/src/main/java/com/xiaan/liangyuan/liangyuanapp/MainActivity.java
@@ -16,9 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.i("slslslsl","hhshshshshshsh");
-        LoggerUtils.i("TAG","oncreste");
-        Logger.t("ksks").i("lslslsllslslslsls");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
