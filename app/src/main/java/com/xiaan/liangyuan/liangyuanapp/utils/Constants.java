87c43c270ab263414e810162ead0fce13b35391b
Kevin Duan <agonyice0115@gmail.com>
2017-11-01 22:29:57 +0800

    test push

--- app/src/main/java/com/xiaan/liangyuan/liangyuanapp/utils/Constants.java
+++ app/src/main/java/com/xiaan/liangyuan/liangyuanapp/utils/Constants.java
@@ -28,6 +28,5 @@ public class Constants {
 	private static final int STATE_CONNECTING = 1;
 	private static final int STATE_CONNECTED = 2;
 	private static final int STATE_DISCONNECTING = 4;
-
 	
 }
